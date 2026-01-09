a86bad9557bae9bd59170602f1263a442072db67
Crolloh <latrellbang@gmail.com>
2026-01-09 15:16:19 +0800

    minor change

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #startup 
 import sys
 from PyQt5.QtWidgets import QApplication
-from gui import AttendanceUI
+from gui import mainWindow
 from attendance import init_db
 
 if __name__ == "__main__":
     init_db()  
 
     app = QApplication(sys.argv)
-    window = AttendanceUI()
+    window = mainWindow()
     window.show()
     sys.exit(app.exec_())

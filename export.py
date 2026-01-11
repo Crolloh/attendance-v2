a858a6a4744d5c0badb1279b344bbff430978cb2
Crolloh <latrellbang@gmail.com>
2026-01-11 23:56:46 +0800

    made exporting as a function

--- export.py
+++ export.py
@@ -1,9 +1,9 @@
 import sqlite3
 import pandas as pd
 
-conn = sqlite3.connect("attendance.db")
-df = pd.read_sql_query("SELECT * FROM attendance", conn)
-df.to_excel("attendance.xlsx", index=False)
-conn.close()
-
-print("Exported to attendance.xlsx")
+def export_excel():
+    conn = sqlite3.connect("attendance.db")
+    df = pd.read_sql_query("SELECT * FROM attendance", conn)
+    df.to_excel("attendance.xlsx", index=False)
+    conn.close()
+    print("Exported to attendance.xlsx")

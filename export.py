8febe19cf37054c4044862f9bafab4592fcb3702
Crolloh <latrellbang@gmail.com>
2026-01-12 15:50:56 +0800

    added exporting to excel with .exe consideration

--- export.py
+++ export.py
@@ -1,9 +1,30 @@
 import sqlite3
 import pandas as pd
+import os
+import sys
+import datetime
+
+def get_db_path():
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, "attendance.db")
+    return "attendance.db"
 
 def export_excel():
-    conn = sqlite3.connect("attendance.db")
-    df = pd.read_sql_query("SELECT * FROM attendance", conn)
-    df.to_excel("attendance.xlsx", index=False)
+    conn = sqlite3.connect(get_db_path())
+
+    query = """
+        SELECT student_id,
+               student_names,
+               time_in,
+               status
+        FROM attendance
+        ORDER BY student_names
+    """
+
+    df = pd.read_sql_query(query, conn)
     conn.close()
-    print("Exported to attendance.xlsx")
+
+    filename = f"Attendance_{datetime.date.today()}.xlsx"
+    df.to_excel(filename, index=False)
+
+    return filename

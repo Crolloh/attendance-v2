e1de394070cae2af7de66346b7157b2800d33814
Crolloh <latrellbang@gmail.com>
2026-01-31 00:47:58 +0800

    exporting bug fixes

--- export.py
+++ export.py
@@ -3,21 +3,23 @@ import pandas as pd
 import os
 import sys
 import datetime
+from attendance import get_gradelevel
 
 def get_db_path():
     if hasattr(sys, "_MEIPASS"):
         return os.path.join(sys._MEIPASS, "attendance.db")
     return "attendance.db"
 
-def export_excel():
+def export_excel(grade_level):
     conn = sqlite3.connect(get_db_path())
-
-    query = """
+    table = get_gradelevel(int(grade_level))
+    query = f"""
         SELECT student_id,
                student_names,
                time_in,
+               time_out,
                status
-        FROM attendance
+        FROM {table}
         ORDER BY student_names
     """
 
